31aa179dbc0477888c1ad1206d053aff0e4a21e7
Stu Schwartz <stupschwartz@gmail.com>
2013-07-17 21:55:18 -0400

    started the exercises for week 0

--- matrix/python_lab.py
+++ matrix/python_lab.py
@@ -13,67 +13,67 @@ y = 1/2
 statement_val = 2**(y+1/2) if x+10<0 else 2**(y-1/2)
 
 ## Task 5
-first_five_squares = { ... for _ in {1,2,3,4,5} }
+first_five_squares = { x**2 for x in {1,2,3,4,5} }
 
 ## Task 6
-first_five_pows_two = { ... for _ in {0,1,2,3,4} }
+first_five_pows_two = { x**2 for x in {0,1,2,3,4} }
 
 ## Task 7: enter in the two new sets
-X1 = { ..., ..., ... }
-Y1 = { ..., ..., ... }
+X1 = { 1, 2, 3 }
+Y1 = { 5, 3, 7 }
 
 ## Task 8: enter in the two new sets
-X2 = { ..., ..., ... }
-Y2 = { ..., ..., ... }
+X2 = { 1, 2, 3 }
+Y2 = { 4, 5, 6 }
 
 ## Task 9
 base = 10
 digits = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-three_digits_set = { ... }
+three_digits_set = { (x*y)+x for x in digits for y in range(base)}
 
 ## Task 10
 S = {1, 2, 3, 4}
 T = {3, 4, 5, 6}
-S_intersect_T = { ... }
+S_intersect_T = { s for s in S if s in T }
 
 ## Task 11
-L_average = ... # average of: [20, 10, 15, 75]
+L_average = sum([20, 10, 15, 75])/4
 
 ## Task 12
 LofL = [[.25, .75, .1], [-1, 0], [4, 4, 4, 4]]
-LofL_sum = ... # use form: sum([sum(...) ... ])
+LofL_sum = sum([sum(l) for l in LofL ])
 
 ## Task 13
-cartesian_product = ... # use form: [ ... {'A','B','C'} ... {1,2,3} ... ]
+cartesian_product = [ l,n for l in {'A','B','C'} for n in {1,2,3} ]
 
 ## Task 14
 S = {-4, -2, 1, 2, 5, 0}
-zero_sum_list = [ ... ] 
+zero_sum_list = [ i,j,k for i in S for j in S for k in S if i+j+k == 0 ] 
 
 ## Task 15
-exclude_zero_list = [ ... ]
+exclude_zero_list = [ i,j,k for i,j,k in zero_sum_list if not i == 0 and j == 0 and k == 0 ]
 
 ## Task 16
-first_of_tuples_list = ...
+first_of_tuples_list = exclude_zero_list[0]
 
 ## Task 17
-L1 = [...] # <-- want len(L1) != len(list(set(L1)))
-L2 = [...] # <-- same len(L2) == len(list(set(L2))) but L2 != list(set(L2))
+L1 = [1,2,2,3] # <-- want len(L1) != len(list(set(L1)))
+L2 = [10,3,5,2,6,4] # <-- same len(L2) == len(list(set(L2))) but L2 != list(set(L2))
 
 ## Task 18
-odd_num_list_range = {...}
+odd_num_list_range = { i for i in range(100) if i % 2 == 1}
 
 ## Task 19
 L = ['A','B','C','D','E']
-range_and_zip = ...
+range_and_zip = zip( range(len(L)), L)
 
 ## Task 20
-list_sum_zip = [...]
+list_sum_zip = [sum(i) for i in zip([10,25,40],[1,25,20])]]
 
 ## Task 21
 dlist = [{'James':'Sean', 'director':'Terence'}, {'James':'Roger', 'director':'Lewis'}, {'James':'Pierce', 'director':'Roger'}]
 k = 'James'
-value_list = [...]
+value_list = [dlist[i][k] for i,k in enumerate(dlist)]
 
 ## Task 22
 dlist = [{'Bilbo':'Ian','Frodo':'Elijah'},{'Bilbo':'Martin','Thorin':'Richard'}]
